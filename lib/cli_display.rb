e4ede8b6502d9f9d28bbbb025d107722b1ec6346
Makis Otman <makisotman@gmail.com>
2014-06-14 16:08:34 +0100

    CliDisplay prints the board

--- lib/cli_display.rb
+++ lib/cli_display.rb
@@ -7,4 +7,12 @@ class CliDisplay
   def greet_players
     @output.puts "Welcome to Tic Tac Toe!"
   end
+
+  def print_board(grid)
+    @output.puts "#{grid[0]} | #{grid[1]} | #{grid[2]}"
+    @output.puts '--|---|--'
+    @output.puts "#{grid[3]} | #{grid[4]} | #{grid[5]}"
+    @output.puts '--|---|--'
+    @output.puts "#{grid[6]} | #{grid[7]} | #{grid[8]}"
+  end
 end
--- spec/cli_display_spec.rb
+++ spec/cli_display_spec.rb
@@ -1,4 +1,5 @@
 require 'cli_display'
+require 'board'
 
 describe CliDisplay do
   it 'greets the players' do
@@ -6,5 +7,12 @@ describe CliDisplay do
     display = CliDisplay.new(output)
     display.greet_players
     expect(output.string).to eq "Welcome to Tic Tac Toe!\n"
+  end
+
+  it 'prints the board' do
+    output = StringIO.new
+    display = CliDisplay.new(output)
+    display.print_board(Board.new.grid)
+    expect(output.string).to eq "1 | 2 | 3\n--|---|--\n4 | 5 | 6\n--|---|--\n7 | 8 | 9\n"
   end
 end
