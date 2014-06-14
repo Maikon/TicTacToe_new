3464f947627d2439cfa6ac40be1128e6882d6d8e
Makis Otman <makisotman@gmail.com>
2014-06-14 13:16:03 +0100

    Refactored tests and board so that it takes custom grid.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,20 +1,20 @@
 require 'board'
 
 describe Board do
+  let(:board) { Board.new }
+
   it 'has a default grid' do
-    expect(Board.new.grid).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    expect(board.grid).to eq [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   describe '#mark_position' do
     it 'marks a position on grid with an X' do
-      board = Board.new
       board.mark_position(1, 'X')
       expect(board.grid).to eq ['X', 2, 3, 4, 5, 6, 7, 8, 9]
     end
 
     it 'raises an error if the position is taken' do
-      board = Board.new
-      board.mark_position(1, 'X')
+      board = Board.new(['X', 2, 3, 4, 5, 6, 7, 8, 9])
       expect{ board.mark_position(1, 'O') }.to raise_error
     end
   end
