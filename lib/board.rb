5b2e6fa9a2c45a337a6c50ee54b51a090ad86ff0
Makis Otman <makisotman@gmail.com>
2014-06-14 14:16:50 +0100

    Refactored rows and columns removing duplication

--- lib/board.rb
+++ lib/board.rb
@@ -22,15 +22,11 @@ class Board
   end
 
   def rows
-    [ [grid[0], grid[1], grid[2]],
-      [grid[3], grid[4], grid[5]],
-      [grid[6], grid[7], grid[8]] ]
+    grid.each_slice(3).to_a
   end
 
   def columns
-    [ [grid[0], grid[3], grid[6]],
-      [grid[1], grid[4], grid[7]],
-      [grid[2], grid[5], grid[8]] ]
+    rows.transpose
   end
 
   def diagonals
