22b2298584c12c8dc543dfdb14400e2fea51da9e
Makis Otman <makisotman@gmail.com>
2014-06-23 15:36:39 +0100

    minimax algorithm with tests checking various scenarios

--- lib/tictactoe/computer.rb
+++ lib/tictactoe/computer.rb
@@ -4,4 +4,35 @@ class Computer
   def choose_mark(board)
     board.available_moves.length.odd? ? @mark = 'X' : @mark = 'O'
   end
+
+  def minimax(board, depth = 0, score = {})
+    return 10 if board.win_for(mark)
+    return -10 if board.win_for(opponent)
+    return 1 if board.draw?
+
+    board.available_moves.each do |move|
+      board.mark_position(move, board.current_mark)
+      score[move] = minimax(board, depth += 1, {})
+      board.reset_value(move)
+    end
+
+    if depth == board.available_moves.length
+      move = score.max_by { |move, score| score }[0]
+      return move
+    end
+
+    if board.current_mark == mark
+      maximum_score = score.max_by { |move, score| score }[1]
+      return maximum_score
+    else
+      minimum_score = score.min_by { |move, score| score }[1]
+      return minimum_score
+    end
+  end
+
+  private
+
+  def opponent
+    @mark == 'X' ? 'O' : 'X'
+  end
 end
--- spec/tictactoe/computer_spec.rb
+++ spec/tictactoe/computer_spec.rb
@@ -16,4 +16,48 @@ describe Computer do
       expect(computer.mark).to eq 'X'
     end
   end
+
+  describe '#minimax' do
+    it 'returns winning move' do
+      board = Board.new(['X', 'X', 3, 'O', 5, 6, 'O', 8, 9])
+      computer = Computer.new
+      computer.choose_mark(board)
+      expect(computer.minimax(board)).to eq 3
+    end
+
+    it 'returns move preventing loss' do
+      board = Board.new([1, 'O', 3, 'X', 5, 6, 'X', 8, 9])
+      computer = Computer.new
+      computer.choose_mark(board)
+      expect(computer.minimax(board)).to eq 1
+    end
+
+    it 'returns move preventing diagonal fork' do
+      board = Board.new(['X', 2, 3, 4, 'O', 6, 7, 8, 'X'])
+      computer = Computer.new
+      computer.choose_mark(board)
+      expect(computer.minimax(board)).to eq 2
+    end
+
+    it 'returns move preventing alternative diagonal fork' do
+      board = Board.new(['O', 2, 3, 4, 'X', 6, 7, 8, 'X'])
+      computer = Computer.new
+      computer.choose_mark(board)
+      expect(computer.minimax(board)).to eq 3
+    end
+
+    it 'returns move preventing edge trap' do
+      board = Board.new([1, 'X', 3, 'X', 'O', 6, 7, 8, 9])
+      computer = Computer.new
+      computer.choose_mark(board)
+      expect(computer.minimax(board)).to eq 1
+    end
+
+    it 'returns move preventing reverse edge trap' do
+      board = Board.new([1, 2, 3, 4, 'O', 'X', 7, 'X', 9])
+      computer = Computer.new
+      computer.choose_mark(board)
+      expect(computer.minimax(board)).to eq 3
+    end
+  end
 end
