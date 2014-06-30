f7e86823571631126b19656b43058a88ae605297
Makis Otman <makisotman@gmail.com>
2014-06-30 13:04:14 +0100

    added initial constructor and executable

--- lib/gui_display.rb
+++ lib/gui_display.rb
@@ -2,24 +2,23 @@ require 'qt'
 
 class GuiDisplay < Qt::Widget
 
+  def initialize
+    super
+    layout = Qt::VBoxLayout.new(self)
+    buttons_layout = Qt::HBoxLayout.new
+    layout.add_widget(greet_players)
+    layout.add_layout(buttons_layout)
+    choice_1 = Qt::PushButton.new(self)
+    buttons_layout.add_widget(choice_1)
+    choice_1.text = 'Human Vs Human'
+    choice_2 = Qt::PushButton.new(self)
+    choice_2.text = 'Human Vs Computer'
+    buttons_layout.add_widget(choice_2)
+  end
+
   def greet_players
     label = Qt::Label.new(self)
     label.text = "Welcome to Tic Tac Toe!"
-  end
-
-  def ask_for_game_type
-
-  end
-
-  def show_board(grid)
-
-  end
-
-  def ask_for_move(board)
-    true
-  end
-
-  def invalid_move_message
-
+    label
   end
 end
--- spec/gui_spec.rb
+++ spec/gui_spec.rb
@@ -2,21 +2,27 @@ require 'gui_display'
 require 'qt'
 
 describe GuiDisplay do
-  it 'is an instance of Qt::Widget' do
+  let(:window) { GuiDisplay.new }
+
+  before(:all) do
     app = Qt::Application.new(ARGV)
-    window = GuiDisplay.new
+  end
+
+  it 'is an instance of Qt::Widget' do
     expect(window).to be_kind_of(Qt::Widget)
   end
 
   it 'greets the players' do
-    app = Qt::Application.new(ARGV)
-    window = GuiDisplay.new
     text = "Welcome to Tic Tac Toe!"
     window.greet_players
-    expect(find_child_with_text(window, text)).to eq true
+    expect(find_label_with_text(window, text)).to eq true
   end
 
-  def find_child_with_text(window, text)
+  it 'asks user for game type selection' do
+    expect(window.find_children(Qt::PushButton).length).to eq 2
+  end
+
+  def find_label_with_text(window, text)
     window.find_children(Qt::Label).any? do |child|
       child.text == text
     end
