e583d705ab0a61e2933c98f8e681fb0b37fc79f2
Makis Otman <makisotman@gmail.com>
2014-06-30 13:40:09 +0100

    Refactored the constructor by extracting into smaller methods

--- lib/gui_display.rb
+++ lib/gui_display.rb
@@ -4,16 +4,28 @@ class GuiDisplay < Qt::Widget
 
   def initialize
     super
+    layout_setup
+    add_button_one
+    add_button_two
+  end
+
+  def layout_setup
     layout = Qt::VBoxLayout.new(self)
-    buttons_layout = Qt::HBoxLayout.new
+    @buttons_layout = Qt::HBoxLayout.new
     layout.add_widget(greet_players)
-    layout.add_layout(buttons_layout)
+    layout.add_layout(@buttons_layout)
+  end
+
+  def add_button_one
     choice_1 = Qt::PushButton.new(self)
-    buttons_layout.add_widget(choice_1)
     choice_1.text = 'Human Vs Human'
+    @buttons_layout.add_widget(choice_1)
+  end
+
+  def add_button_two
     choice_2 = Qt::PushButton.new(self)
     choice_2.text = 'Human Vs Computer'
-    buttons_layout.add_widget(choice_2)
+    @buttons_layout.add_widget(choice_2)
   end
 
   def greet_players
