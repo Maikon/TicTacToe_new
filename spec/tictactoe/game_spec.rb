807603956966f03fc75f3dac624a8e2208cf669d
Makis Otman <makisotman@gmail.com>
2014-07-08 11:49:25 +0100

    Removed duplication from game tests

--- spec/tictactoe/game_spec.rb
+++ spec/tictactoe/game_spec.rb
@@ -3,6 +3,9 @@ require 'tictactoe/board'
 require 'spec_helper'
 
 describe TicTacToe::Game do
+  let(:board) { TicTacToe::Board.new }
+  let(:game)  { TicTacToe::Game.new(board) }
+
   describe '#winner?' do
     it 'returns true if there is a winner' do
       board = TicTacToe::Board.new(['X', 'X', 'X', 4, 5, 6, 7, 8, 9])
@@ -11,8 +14,6 @@ describe TicTacToe::Game do
     end
 
     it 'returns false if there is no winner' do
-      board = TicTacToe::Board.new
-      game = TicTacToe::Game.new(board)
       expect(game.winner?).to eq false
     end
   end
@@ -25,8 +26,6 @@ describe TicTacToe::Game do
     end
 
     it 'returns false if the game is not a draw' do
-      board = TicTacToe::Board.new
-      game = TicTacToe::Game.new(board)
       expect(game.draw?).to eq false
     end
   end
