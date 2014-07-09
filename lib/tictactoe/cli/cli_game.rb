5f891ff3e29529a1833c992b9db280ff1df06576
Makis Otman <makisotman@gmail.com>
2014-07-09 17:10:52 +0100

    CliGame method validating move

--- lib/tictactoe/cli/cli_game.rb
+++ lib/tictactoe/cli/cli_game.rb
@@ -14,5 +14,14 @@ module TicTacToe
     def print_board
       display.show_board(game.board_grid)
     end
+
+    def validate_move
+      move = display.ask_for_move(game.current_mark)
+      until game.valid_move?(move)
+        display.invalid_move_message
+        move = display.ask_for_move(game.current_mark)
+      end
+      move
+    end
   end
 end
--- spec/tictactoe/cli/cli_game_spec.rb
+++ spec/tictactoe/cli/cli_game_spec.rb
@@ -3,13 +3,13 @@ require 'tictactoe/cli/cli_display'
 
 describe TicTacToe::CliGame do
     let(:output)  { StringIO.new }
-    let(:input)   { StringIO.new }
+    let(:input)   { StringIO.new("X\n2\n") }
     let(:display) { TicTacToe::CliDisplay.new(output, input) }
     let(:game)    { TicTacToe::Game.new }
+    let(:runner)  { TicTacToe::CliGame.new(display, game) }
 
   describe '#start' do
     it 'greets the players' do
-      runner = TicTacToe::CliGame.new(display, game)
       runner.start
       expect(output.string).to eq "***************\nWelcome to Tic Tac Toe!\n***************\n"
     end
@@ -17,9 +17,14 @@ describe TicTacToe::CliGame do
 
   describe '#print_board' do
     it 'prints the board' do
-      runner = TicTacToe::CliGame.new(display, game)
       runner.print_board
       expect(output.string).to eq "1 | 2 | 3\n--|---|--\n4 | 5 | 6\n--|---|--\n7 | 8 | 9\n"
+    end
+  end
+
+  describe '#validate_move' do
+    it 'keeps asking user input until move is valid' do
+      expect(runner.validate_move).to eq 2
     end
   end
 end
