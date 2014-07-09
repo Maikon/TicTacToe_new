c92ba99727bfae60e7141107bb3e861d84658ea5
Makis Otman <makisotman@gmail.com>
2014-07-09 10:46:05 +0100

    renamed and refactored move sequence

--- lib/tictactoe/cli/cli_game.rb
+++ lib/tictactoe/cli/cli_game.rb
@@ -10,22 +10,26 @@ module TicTacToe
     def start
       display.greet_players
       until game.over?
-        move_sequence
+        make_move
       end
     end
 
-    def move_sequence
-      move = receive_user_input
-      until game.valid_move?(move)
-        display.invalid_move_message
-        move = receive_user_input
-      end
-      game.play_next_move(move)
+    def make_move
+      game.play_next_move(validate_move(receive_user_input))
     end
 
     def receive_user_input
       display.show_board(game.board_grid)
       display.ask_for_move(game.current_mark)
+    end
+    private
+
+    def validate_move(move)
+      until game.valid_move?(move)
+        display.invalid_move_message
+        move = receive_user_input
+      end
+      move
     end
   end
 end
