ae9f1631fe73a7805cff9d48e7ccb5ca5d65b8fc
Makis Otman <makisotman@gmail.com>
2014-07-17 16:04:16 +0100

    Display updates appropriate cell when click event occurs

--- lib/tictactoe/gui/display.rb
+++ lib/tictactoe/gui/display.rb
@@ -1,18 +1,26 @@
 require 'qt'
 require 'tictactoe/gui/cell'
+require 'tictactoe/game'
 
 module TicTacToe
   module GUI
     class Display < Qt::Widget
-      attr_reader :grid
+      attr_reader :grid, :game
 
       def initialize
         super
+        @game = Game.new
         build_grid
       end
 
       def build_grid
-        @grid = Array.new(9) { Cell.new(self, nil) }
+        @grid = Array.new(9) { Cell.new(self, game) }
+      end
+
+      def play(object)
+        object.setText(game.current_mark)
+        position = grid.index(object) + 1
+        game.play_next_move(position)
       end
     end
   end
--- spec/tictactoe/gui/display_spec.rb
+++ spec/tictactoe/gui/display_spec.rb
@@ -12,4 +12,12 @@ describe TicTacToe::GUI::Display do
       expect(cell).to be_instance_of(TicTacToe::GUI::Cell)
     end
   end
+
+  it 'marks the appropriated cell when clicked' do
+    display = TicTacToe::GUI::Display.new
+    display.grid[0].mousePressEvent(nil)
+    expect(display.grid[0].text).to eq 'X'
+    display.grid[1].mousePressEvent(nil)
+    expect(display.grid[1].text).to eq 'O'
+  end
 end
